616e27cd4fddfe017be35f912ef8e633e61f0de6
Cameron Whitehead <camjw119@gmail.com>
2018-10-31 18:06:26 +0000

    Fixed SettingsController spec

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe SettingsController, type: :controller do
     it 'updates user settings' do
       user = User.create(email: 'test@mail.com', password: 'password',
                          font: 'Lobster', background_colour: '#FFFFFF')
-      post :update_settings, params: { profile:
+      post :update_settings, params: { setting:
         { font: 'Times New Roman', background_colour: '#FF0000',
           userId: user.id } }
       expect(response).to have_http_status(:success)
