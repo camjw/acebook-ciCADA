4ee6093b2e486703db11ea8d88887627074e6939
Alex Kharouk <akharoukpp@gmail.com>
2018-10-31 15:34:09 +0000

    write test to maintain a fantastic 100% coverage

---
+++ spec/features/user_can_visit_their_username_path_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Username Route', type: :feature do
+  scenario 'Can visit their route with username' do
+    signup
+    visit '/test'
+    expect(page).to have_content("Welcome to Your profile test")
+  end
+end
