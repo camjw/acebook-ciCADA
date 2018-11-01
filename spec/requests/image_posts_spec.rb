164505d5f69fda63e03209a2e0261ede1fefe2e9
Alex Kharouk <akharoukpp@gmail.com>
2018-11-01 15:02:31 +0000

    pass 3/4 tests

--- spec/requests/image_posts_spec.rb
+++ spec/requests/image_posts_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'ImagePosts', type: :request do
   describe 'GET /image_posts' do
     it 'works! (now write some real specs)' do
       get image_posts_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(302)
     end
   end
 end
