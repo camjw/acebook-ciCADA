66f0c6d69474a5902e13b8cac1e64bdb36084774
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-02 12:17:13 +0000

    Fixed login bug, edited landing page, passed RSpec

--- spec/features/user_who_isnt_signed_in_spec.rb
+++ spec/features/user_who_isnt_signed_in_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.feature 'Timeline', type: :feature do
   scenario 'Can not see since not logged in' do
     visit '/'
-    expect(page).to have_content('Log in')
+    expect(page).to have_content('Login')
   end
 end
