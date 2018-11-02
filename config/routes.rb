382dc13ae956400ccbb75ff9921e4a783b2e05b4
Alex Kharouk <akharoukpp@gmail.com>
2018-11-02 13:16:30 +0000

    add routes to the exclusion crew of the block length

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do 
-  # rubocop:disable Metrics/BlockLength
+Rails.application.routes.draw do
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
   get 'rooms/show', to: 'rooms#show'
